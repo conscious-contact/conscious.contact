0fb71ea962bf29564665ceb2bea2c9ac62a1b3a6
Nic Johnson <233067+nicjohnson@users.noreply.github.com>
2024-09-18 16:50:53 -0600

    Update Algolia approach, templating

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
+const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
+require("dotenv").config();
+
 module.exports = function(eleventyConfig){
   eleventyConfig.addPassthroughCopy("src/assets/*/**");
+  eleventyConfig.addPlugin(EleventyVitePlugin);
 
   return {
     dir: {
