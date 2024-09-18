4d4e021d2465dab51593b1490917332bc6e1b31a
Nic Johnson <233067+nicjohnson@users.noreply.github.com>
2024-09-18 16:56:34 -0600

    Remove Vite plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,9 @@
-const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
+// const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
 require("dotenv").config();
 
 module.exports = function(eleventyConfig){
   eleventyConfig.addPassthroughCopy("src/assets/*/**");
-  eleventyConfig.addPlugin(EleventyVitePlugin);
+  // eleventyConfig.addPlugin(EleventyVitePlugin);
 
   return {
     dir: {
