0923dfc3b6c45c657812dc34205656e53a4f4fb8
Nic Johnson <iheartredsox@gmail.com>
2024-10-31 09:50:36 -0600

    Add robots.txt and sitemap.xml

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 
 module.exports = function(eleventyConfig){
   eleventyConfig.addPassthroughCopy("src/assets/*/**");
+  eleventyConfig.addPassthroughCopy("src/robots.txt", "/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/sitemap.xml", "/sitemap.xml");
   // eleventyConfig.addPlugin(EleventyVitePlugin);
 
   return {
